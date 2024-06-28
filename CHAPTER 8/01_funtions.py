ca73d8e44fbd1de87cf3cc80104dea689b5ce255
bhandwalkardarshan <bhandwalkardarshan@gmail.com>
2024-06-28 21:16:46 +0530

    functions and recursion

---
+++ CHAPTER 8/01_funtions.py
@@ -0,0 +1,9 @@
+def avg():                  # function definition
+    a = int(input("Enter number a: "))
+    b = int(input("Enter number b: "))
+    c = int(input("Enter number c: "))
+
+    avg = (a + b + c)/3
+    return avg
+
+print(avg())    # function call 
---
+++ CHAPTER 8/03_function_agruments.py
@@ -0,0 +1,5 @@
+def goodDay(name):
+    print(f"Have a good day, {name}")
+
+name = input("Enter Your name: ")
+goodDay(name) 
